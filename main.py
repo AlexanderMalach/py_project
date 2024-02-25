f1aa11b2707dc071a88f132affe7fff02e5e0919
79096 <a.malakhinsky@gmail.com>
2024-02-25 23:01:43 +0300

    import subtraction call

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from funcs import add_two_numbers
 
 resalt_add = add_two_numbers(5, 3)
 print(resalt_add)
+
+
+from funcs import subtraction
+
+resalt_subtract = subtraction(5, 3)
+print(resalt_subtract)
